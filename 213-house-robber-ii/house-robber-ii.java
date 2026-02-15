47d08ea96b0cec73b0a791814a310cb75a3f5ed8
madhumitha-b <87955195+madhumitha-b@users.noreply.github.com>
2026-02-15 15:56:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (60.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,22 +1,39 @@
 class Solution {
+    int recursion(int[] nums){
 
-public int rob(int[] nums) {
-    if (nums.length == 1) return nums[0];
-    
-    return Math.max(robLinear(nums, 0, nums.length - 2),  // Skip last
-                    robLinear(nums, 1, nums.length - 1)); // Skip first
-}
+        int n=nums.length;
+        int prev=nums[0];
+        int prev2=0;
 
-private int robLinear(int[] nums, int start, int end) {
-    int prev2 = 0, prev1 = 0;
-    for (int i = start; i <= end; i++) {
-        int take = nums[i] + prev2;
-        int skip = prev1;
-        int curr = Math.max(take, skip);
-        prev2 = prev1;
-        prev1 = curr;
+        for(int i=0;i<n;i++){
+            int take=nums[i];
+
+            if(i>1) take +=prev2;
+            int nottake = 0+prev;
+
+            int curi=Math.max(take,nottake);
+            prev2=prev;
+            prev=curi;
+        }
+        return prev;
     }
-    return prev1;
-}
 
+    public int rob(int[] nums) {
+
+        int[] ver1=new int[nums.length];
+        int[] ver2=new int[nums.length];
+        if(nums.length==1)return nums[0];
+        int k=0;
+        for(int i=1;i<nums.length;i++){
+            ver1[k++]=nums[i];
+        }
+        k=0;
+        for(int i=0;i<nums.length-1;i++){
+            ver2[k++]=nums[i];
+        }
+
+        int a= recursion(ver1);
+        int b= recursion(ver2);
+        return Math.max(a,b);
+    }
 }
