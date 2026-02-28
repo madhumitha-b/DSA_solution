7f10552fb0d31c2e67d7ced54e6e68a2b8d7174d
madhumitha-b <87955195+madhumitha-b@users.noreply.github.com>
2026-03-01 00:42:14 +0530

    Time: 5 ms (99.72%) | Memory: 42.8 MB (95.84%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+
+public int change(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;  // 1 way to make amount 0
+    
+    for (int coin : coins) {
+        for (int x = coin; x <= amount; x++) {
+            dp[x] += dp[x - coin];
+        }
+    }
+    return dp[amount];
+}
+
+}
