7e7f9e6be1b1f01c39400df09664cb3be3abc48b
madhumitha-b <87955195+madhumitha-b@users.noreply.github.com>
2026-07-27 10:54:23 +0530

    Time: 2 ms (96.44%) | Memory: 45.8 MB (51.70%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,30 +2,32 @@ class Solution {
 
     List<List<Integer>> res = new ArrayList<>();
 
-    void subsequence(int index, int[] candidates, int target, List<Integer> lst, int sum) {
+    void subset(int index,List<Integer> arr,int[] candidates,int sum,int target){
 
-        if (sum == target) {
-            res.add(new ArrayList<>(lst)); // copy list
-            return;
+        if(index>=candidates.length){
+            if(target == sum){
+            res.add(new ArrayList<>(arr));
+            }
+            return ;
         }
-
-        if (sum > target || index == candidates.length) {
-            return;
+        if(target == sum){
+            res.add(new ArrayList<>(arr));
+            return ;
         }
-
-        // Take current element (can reuse)
-        lst.add(candidates[index]);
-        subsequence(index, candidates, target, lst, sum + candidates[index]);
-
-        // Backtrack
-        lst.remove(lst.size() - 1);
-
-        // Skip current element
-        subsequence(index + 1, candidates, target, lst, sum);
+        if(sum>target) return ;
+        arr.add(candidates[index]);
+        sum=sum+candidates[index];
+        subset(index,arr,candidates,sum,target);
+        sum=sum-candidates[index];
+        arr.remove(arr.size()-1);
+        subset(index+1,arr,candidates,sum,target);
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        subsequence(0, candidates, target, new ArrayList<>(), 0);
+        
+        List<Integer> arr = new ArrayList<>();
+
+        subset(0,arr,candidates,0,target);
         return res;
     }
 }
